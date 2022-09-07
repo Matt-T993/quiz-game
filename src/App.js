5e8ad5e5fe484fc35cb7b83c02937819e933e33a
MattChu993 <53846145+MattChu993@users.noreply.github.com>
2022-09-07 14:52:23 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import QuizCard from "./components/QuizCard";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz game</h1>
+      <h2>Score: 2</h2>
+      <QuizCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuizCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./quizcard.scss";
+
+const QuizCard = () => {
+  return (
+    <div className="question-card">
+      <div className="question-list">
+        <p className="question-item">Manga</p>
+        <p className="question-item">Manga</p>
+        <p className="question-item">Manga</p>
+        <p className="question-item">Manga</p>
+      </div>
+    </div>
+  );
+};
+
+export default QuizCard;
